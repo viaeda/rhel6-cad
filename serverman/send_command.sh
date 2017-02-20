f74ba25c990827c521497283bf7fe8d418f0c3d2
Charles Wu <charles.wu@viaeda.com>
2017-02-20 16:58:52 +0800

    add server maintain script dir

---
+++ serverman/send_command.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# File Name: send_command.sh
+# Author: Charles Wu <charles.wu@wztek.com>
+# Created Date: 2017-02-20
+# Last Modified: 2017-02-20 16:56:55
+# Tel: +86-13917126915
+# REV: 
+
+
+hosts_list=("brix" "brix");
+command=$1;
+echo $command;
+
+if [ -z "$command" ];then
+	echo "please input command like 'send_command hostname'";
+else
+	for hostname in ${hosts_list[*]}
+	do
+	        echo "This is $hostname";
+	        ssh $hostname "$command";
+	done
+fi
+
