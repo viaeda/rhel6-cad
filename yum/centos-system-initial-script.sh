657148cd198f8b54ac3c7c7a377598573283fd49
Qi Wu <qi.wu@outlook.com>
2020-01-31 17:32:29 +0800

    update yum to centos 6.10 2nd

---
+++ yum/centos-system-initial-script.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# File Name: rhel6-system-initial-script.sh
+# Author: Charles Wu <qi.wu@outlook.com>
+# Created Date: 2016-10-07
+# Last Modified: 2018-03-28 15:38:18
+# Tel: +86-13917126915
+# REV: 
+
+
+
+
+## Change teminal default directory to $HOME
+gconftool-2 --direct --config-source xml:readwrite:/etc/gconf/gconf.xml.defaults --set --type=bool /apps/nautilus-open-terminal/desktop_opens_home_dir true
+
+
+
+## Set New Windows Alway On Top
+gconftool-2 --direct --config-source xml:readwrite:/etc/gconf/gconf.xml.defaults --set --type=bool /apps/metacity/general/new_windows_always_on_top true
+
+
+
+## turn off authentucation window popup in VNC Desktop
+policy_file=/usr/share/polkit-1/actions/org.freedesktop.packagekit.policy
+cp $policy_file ~/policy_auth.bak
+
+sed -i 's/auth_admin<\/allow_any>/no<\/allow_any>/g' $policy_file
+sed -i 's/auth_admin<\/allow_inactive>/no<\/allow_inactive>/g' $policy_file
+
+
+
+## turn off display user on login screen
+gconftool-2 --direct --config-source xml:readwrite:/etc/gconf/gconf.xml.defaults --set --type=bool /apps/gdm/simple-greeter/disable_user_list true
+
+
+
+
+## turnoff iptables
+/sbin/service iptables stop
+/sbin/chkconfig iptables off
+
+
+
+## Disable SELINUX
+/usr/sbin/setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+
+
+
+
+/sbin/chkconfig cloud-config off
+/sbin/chkconfig cloud-final off
+/sbin/chkconfig cloud-init off
+/sbin/chkconfig cloud-init-local off
+/sbin/chkconfig ctdb off
+/sbin/chkconfig ipmidetectd off
+/sbin/chkconfig cgdcbxd off
+/sbin/chkconfig systemtap off
+
+yum remove esc -y
--- yum/yuminstall.sh
+++ yum/yuminstall.sh
@@ -4,7 +4,7 @@
 # Author: Charles Wu <qi.wu@outlook.com>
 # Created Date: 2016-08-29
 # Last Modified: 2018-03-28 15:49:24
-# Tel: +86-18601683566
+# Tel: +86-13917126915
 # REV: 1.0
 
 
@@ -12,11 +12,3 @@ while read line
 do
   yum groupinstall "$line" -y
 done < groups.list
-
-
-## Remove subscription pop-up
-yum remove subscription-manager -y
-yum remove subscription-manager-migration-data -y
-
-## Remove Smart Card pop-up
-yum remove esc -y
